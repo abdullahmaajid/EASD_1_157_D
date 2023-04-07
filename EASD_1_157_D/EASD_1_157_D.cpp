accb299380699643ac2b85f5a07c78262db87b8c
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-07 10:23:52 +0700

    memasukkan fungsi algoritma

--- EASD_1_157_D/EASD_1_157_D.cpp
+++ EASD_1_157_D/EASD_1_157_D.cpp
@@ -46,6 +46,25 @@ void input(int arr[], int size) {
 	}
 }
 
+// Algoritma Selection Sort
+void selectionSort(int arr[], int size) {
+    string initials = "AM";                                                     // inisial dari nama lengkap saya [Abdullah Maajid]
+    for (int initialsIndex = 0; initialsIndex < size - 1; initialsIndex++) {
+        int min_index = initialsIndex;
+        for (int i = initialsIndex + 1; i < size; i++) {
+            if (arr[i] < arr[min_index]) {
+                min_index = i;
+            }
+        }
+        if (min_index != initialsIndex) {
+            swap(arr[initialsIndex], arr[min_index]);
+        }
+    }
+}
+
+
+
+
 
 
 	
