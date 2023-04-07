ca75d8c7a86726568462e147c2b1a21b632f521a
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-07 09:08:11 +0700

    Add project files.

---
+++ EASD_1_157_D/EASD_1_157_D.cpp
@@ -0,0 +1,2 @@
+// PERTANYAAN : Mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah?
+// JAWABAN    : Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan gabungan langkah-langkah yang digunakan untuk menyelesaikan masalah. 
