2b58dac3ea0978030180dea963729b950978d6cb
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-07 09:09:14 +0700

    jawaban nomor 1

--- EASD_1_157_D/EASD_1_157_D.cpp
+++ EASD_1_157_D/EASD_1_157_D.cpp
@@ -1,2 +1,4 @@
 // PERTANYAAN : Mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah?
 // JAWABAN    : Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan gabungan langkah-langkah yang digunakan untuk menyelesaikan masalah. 
+
+//
