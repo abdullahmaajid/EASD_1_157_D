919b8ef65bc79c3ac489a92d2c347177e195a719
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-07 09:17:50 +0700

    jawab pertanyaan no. 4

--- EASD_1_157_D/EASD_1_157_D.cpp
+++ EASD_1_157_D/EASD_1_157_D.cpp
@@ -1,8 +1,11 @@
-// PERTANYAAN : Mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah?
-// JAWAB      : Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan gabungan langkah-langkah yang digunakan untuk menyelesaikan masalah. 
+// PERTANYAAN 1 : Mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah?
+// JAWAB        : Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan gabungan langkah-langkah yang digunakan untuk menyelesaikan masalah. 
 
-// PERTANYAAN : Dalam algorithma, di klassifikasikan menjadi 2 data struktur. Sebutkan 2 data struktur tsb!
-// JAWAB      : 2 Data Struktur tersebut yaitu Linear Data Structure dan Non-Linear Data Structure
+// PERTANYAAN 2 : Dalam algorithma, di klassifikasikan menjadi 2 data struktur. Sebutkan 2 data struktur tsb!
+// JAWAB        : 2 Data Struktur tersebut yaitu Linear Data Structure dan Non-Linear Data Structure
 
-// PERTANYAAN : Sebutkan faktor-faktor yang mempengaruhi mempengaruhi efisiensi suatu program algorithma!
-// JAWAB	  : 6 Faktor yang mempengaruhi efisiensi suatu data progam algorithma yaitu Ukuran Input, Struktur Data, Kompleksitas Algoritma, Penggunaan Memory, Bahasa Pemrograman, Algoritma yang Dipilih 
+// PERTANYAAN 3 : Sebutkan faktor-faktor yang mempengaruhi mempengaruhi efisiensi suatu program algorithma!
+// JAWAB	    : 6 Faktor yang mempengaruhi efisiensi suatu data progam algorithma yaitu Ukuran Input, Struktur Data, Kompleksitas Algoritma, Penggunaan Memory, Bahasa Pemrograman, Algoritma yang Dipilih
+
+// PERTANYAAN 4 : Dari 6 algorithma yang sudah dipelajari. Algortihma manakah yang paling efisien untuk mengurutkan data yang ukurannya kecil.Jelaskan kenapa anda memilih itu.
+// JAWAB	    : Karena Selection Sort memiliki kompleksitas waktu O(n^2) yang relatif lebih cepat dibandingkan dengan algoritma Insertion Sort dan Bubble Sort, yang kompleksitas waktu nya juga O(n^2). Selain itu, Selection Sort juga sederhana dan mudah dipahami dalam implementasinya, sehingga lebih mudah untuk digunakan dalam pengurutan data kecil.
