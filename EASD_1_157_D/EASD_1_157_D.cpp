b2d0b820ca60163643bb336d9a509492bc0b7542
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-07 09:11:57 +0700

    jawab soal no.2

--- EASD_1_157_D/EASD_1_157_D.cpp
+++ EASD_1_157_D/EASD_1_157_D.cpp
@@ -1,4 +1,6 @@
 // PERTANYAAN : Mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah?
 // JAWABAN    : Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan gabungan langkah-langkah yang digunakan untuk menyelesaikan masalah. 
 
-//
+// PERTANYAAN : Dalam algorithma, di klassifikasikan menjadi 2 data struktur. Sebutkan 2 data struktur tsb!
+// JAWABAN	  : 2 Data Struktur tersebut yaitu Linear Data Structure dan Non-Linear Data Structure
+
