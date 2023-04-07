5bbb3f9adf7b986474bc605cfe8a8fe0e033f0d5
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-04-07 09:14:46 +0700

    jawab no.3

--- EASD_1_157_D/EASD_1_157_D.cpp
+++ EASD_1_157_D/EASD_1_157_D.cpp
@@ -1,6 +1,8 @@
 // PERTANYAAN : Mengapa suatu algorithma dibutuhkan untuk menyelesaikan suatu masalah?
-// JAWABAN    : Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan gabungan langkah-langkah yang digunakan untuk menyelesaikan masalah. 
+// JAWAB      : Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma merupakan gabungan langkah-langkah yang digunakan untuk menyelesaikan masalah. 
 
 // PERTANYAAN : Dalam algorithma, di klassifikasikan menjadi 2 data struktur. Sebutkan 2 data struktur tsb!
-// JAWABAN	  : 2 Data Struktur tersebut yaitu Linear Data Structure dan Non-Linear Data Structure
+// JAWAB      : 2 Data Struktur tersebut yaitu Linear Data Structure dan Non-Linear Data Structure
 
+// PERTANYAAN : Sebutkan faktor-faktor yang mempengaruhi mempengaruhi efisiensi suatu program algorithma!
+// JAWAB	  : 6 Faktor yang mempengaruhi efisiensi suatu data progam algorithma yaitu Ukuran Input, Struktur Data, Kompleksitas Algoritma, Penggunaan Memory, Bahasa Pemrograman, Algoritma yang Dipilih 
